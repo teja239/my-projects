27917da8dcc4cd148f80228a237f0aa9a2fa0806
teja <tejachinnu239@gmail.com>
2022-03-04 22:35:50 +0530

    changes made

---
+++ phase 1/assisted projects/collections/collections/collectionAssisted.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.*;
+
+public class collectionAssisted {
+
+	public static void main(String[] args) {
+		//creating arraylist
+		System.out.println("ArrayList");
+		ArrayList<String> city=new ArrayList<String>();   
+	      city.add("Bangalore");//
+	      city.add("Delhi");    	   
+	      System.out.println(city);  
+	}
+}
---
+++ phase 1/assisted projects/collections/collections/vectorAssisted.java
@@ -0,0 +1,13 @@
+package collections;
+import java.util.Vector;
+public class vectorAssisted {
+
+	public static void main(String[] args) {
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector();
+	      vec.addElement(15); 
+	      vec.addElement(30); 
+	      System.out.println(vec);
+
+	}
+}
