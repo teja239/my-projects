687cde384d5a7dc5f38a05fe251b2690922a2894
teja <tejachinnu239@gmail.com>
2022-04-30 10:35:23 +0530

    changes made

--- AdvanceTrainingAllProjects/AdvanceTrainingAllProjects/src/com/teja/problem3_1/Instrument.java
+++
@@ -1,5 +0,0 @@
-package com.teja.problem3_1;
-public abstract class Instrument
-{
-public abstract void Play();
-}
--- AdvanceTrainingAllProjects/AdvanceTrainingAllProjects/src/com/teja/problem7_2/AgeNotWithinRangeException.java
+++
@@ -1,9 +0,0 @@
-package com.teja.problem7_2;
-
-class AgeNotWithInRangeException extends Exception
-{
-     public String toString()
-     {
-          return ("Age is not between 15 and 21. please ReEnter the Age");
-     }
-}
